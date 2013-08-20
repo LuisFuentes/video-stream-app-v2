448a887656602f8a4ee9b7c98b441e0c528b2be7
LuisFuentes <luis90fuentes@gmail.com>
2013-08-19 19:21:29 -0700

    user auth commit

--- videoApp/flaskApp/run.py
+++ videoApp/flaskApp/run.py
@@ -1,24 +1,35 @@
-from flask import render_template, Flask, request, redirect, url_for, abort, session
+from flask import render_template, Flask, request, redirect, url_for, abort, session, flash
+from flask.ext.wtf import Form
+from wtforms import TextField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'L33T!';
+
+class LoginForm(Form):
+	username = TextField('username')
+	password = TextField('password')
+
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("login.html")
 	
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
-	session['username'] = request.form['username']
-	session['password'] = request.form['password']
-	return redirect(url_for('video'))
-
+	form = LoginForm()
+	
+	if (str(form.username.data)) == "luis" and (str(form.password.data)) == "sofiebio":
+		return redirect(url_for('video'))
+	else:
+		return redirect(url_for('index'))
+	
 @app.route('/video')
 def video():
 	return render_template("video.html")
+	
 		
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
 	
 
